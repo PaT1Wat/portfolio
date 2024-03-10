d20d3d38e8b88024fd0beb32c3965215d00a0c6d
PATI <150318162+PaT1Wat@users.noreply.github.com>
2024-03-10 20:06:55 +0700

    Update Portfolio.jsx

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -5,7 +5,7 @@ import Tilt from "react-parallax-tilt";
 function Portfolio() {
 
   return (
-    <div className={styles.port_con}>
+    <div id="Portfolio" className={styles.port_con}>
       <h3 className={styles.port_title}>Portfolio</h3>
       <div className={styles.port_list}>
         
